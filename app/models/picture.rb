ea636f1b3f0c469dc6bbced08379a9b7d1b73620
cyrill62 <cyrill62@Touchy.grep-ivcopyright.homeunix.net>
2009-02-02 13:02:59 +0100

    Add pictures configuration

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,14 +1,7 @@
 class Picture < ActiveRecord::Base
 
   acts_as_commentable
-  has_attachment :storage => :file_system, :file_system_path => 'public/images/sortable_pictures',
-    :content_type => 'image', :thumbnails => { 
-    :big => '500x500', 
-    :normal => '200x200', 
-    :small => '100x100', 
-    :thumb => '50x50'
-  }
-
+  has_attachment(SortablePictures::options)
   validates_presence_of :content_type
 
   has_many :sortable_pictures, :dependent => :destroy
